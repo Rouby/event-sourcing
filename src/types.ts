85ca8b323011f92f073df2301fe379b7d03073c5
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2023-10-19 15:02:30 +0200

    chore: add customizable event props
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- src/types.ts
+++ src/types.ts
@@ -10,12 +10,14 @@ export type RegisteredEvent = RegisterEvents[keyof RegisterEvents] extends never
   ? { type: string; payload: any }
   : RegisterEvents[keyof RegisterEvents];
 
+export interface AdditionalEventProps {}
+
 export type SourcingEvent = RegisteredEvent & {
   id: string;
   causationId: string;
   correlationId: string;
   createdAt: Date;
   verified?: boolean;
-};
+} & AdditionalEventProps;
 
 export type ModelKind = keyof RegisterModels;
