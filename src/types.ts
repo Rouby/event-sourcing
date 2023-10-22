64e485eb6a5a6698645739c5d150d9bbfab5f2d2
Jonathan Burke <jonathan.burke.1311@googlemail.com>
2023-10-22 11:52:05 +0200

    chore: improve type
    Signed-off-by: Jonathan Burke <jonathan.burke.1311@googlemail.com>

--- src/types.ts
+++ src/types.ts
@@ -10,12 +10,10 @@ export type RegisteredEvent = RegisterEvents[keyof RegisterEvents] extends never
   ? { type: string; payload: any }
   : RegisterEvents[keyof RegisterEvents];
 
-export type RegisteredModels = RegisterModels extends Record<
-  string,
-  new (...args: any[]) => any
->
-  ? {}
-  : { [P in keyof RegisterModels]: RegisterModels[P] };
+export type RegisteredModels =
+  RegisterModels[keyof RegisterModels] extends never
+    ? {}
+    : { [P in keyof RegisterModels]: RegisterModels[P] };
 
 export interface AdditionalEventProps {}
 
